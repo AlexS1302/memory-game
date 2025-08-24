ab4276456a58de963f4596ba1311e7227e6e4cd3
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-08-24 16:38:57 +0100

    Implement fetchPokemon() and enhance styling
    -Added pokemon sprites & names to cards
    -Added title and scores to Header

--- src/Game.jsx
+++ src/Game.jsx
@@ -8,15 +8,48 @@ function Game() {
       id: crypto.randomUUID(),
     }));
     setCards(newCards);
-    console.log(newCards);
   }, []);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      try {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=12"
+        );
+        const data = await response.json();
+        console.log("API Data:", data);
+
+        const pokemonList = data.results;
+
+        const detailedCards = await Promise.all(
+          pokemonList.map(async (pokemon) => {
+            const result = await fetch(pokemon.url);
+            const details = await result.json();
+            return {
+              id: details.id,
+              name: details.name,
+              sprite:
+                details.sprites.front_default,
+            };
+          })
+        );
+        setCards(detailedCards);
+      } catch (error) {
+        console.error("Problem fetching Pokemons:", error);
+      }
+    }
+
+    fetchPokemon();
+  }, []);
+
+  console.log("Array:", cards);
 
   return (
     <div className="Game">
       {cards.map((card) => (
         <div key={card.id} className="card">
-          <div className="pokemon-sprite">Sprite</div>
-          <div className="pokemon-name">Name</div>
+          <img className="pokemon-sprite" src={card.sprite}></img>
+          <p className="pokemon-name">{card.name}</p>
         </div>
       ))}
     </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,12 @@
 function Header() {
-  return <div className="Header">Header</div>;
+  return (
+    <div className="Header">
+      <h1 className="header-title">Pokémon Memory Game</h1>
+      <div className="header-scores">
+        <h3>Score:</h3>
+        <h3>Best Score:</h3>
+      </div>
+    </div>
+  );
 }
 export default Header;
